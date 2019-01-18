3266a2a929c61f70c8c27689e69f7d7b3869cdf1
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-01-18 09:11:16 -0800

    implements ex1 solution and passes tests

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -3,13 +3,32 @@
 #include "hashtable.h"
 #include "ex1.h"
 
+
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
+  Answer *answer = malloc(sizeof(Answer *));
+
+  answer->index_1 = -1;
+  answer->index_2 = -1;
 
   // YOUR CODE HERE
+  for (int i = 0; i < length; i++) {
+    hash_table_insert(ht, weights[i], i);
 
-  return NULL;
+    if (answer->index_2 < 0) {
+      answer->index_2 = hash_table_retrieve(ht, limit - weights[i]);
+    }
+    if (answer->index_2 >= 0) {
+      answer->index_1 = hash_table_retrieve(ht, limit - weights[answer->index_2]);
+    }
+  }
+
+  if (answer->index_1 < 0 && answer->index_2 < 0) {
+    return NULL;
+  }
+  free(answer);
+  return answer;
 }
 
 void print_answer(Answer *answer)
